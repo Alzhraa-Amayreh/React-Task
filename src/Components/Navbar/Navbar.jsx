c2d59fee2db659186f63bf148cff079e4f1ed545
Alzhraa-Amayreh <alzhramayrh@gmail.com>
2025-10-28 10:00:38 +0200

    Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import  NavStyle from './Navbar.module.css'
+
+
+function Navbar(){
+
+
+    return <>
+    <nav className={`${NavStyle.navColor} navbar navbar-expand-lg p-4`}>
+  <div className="container">
+    <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active text-light" aria-current="page" href="#">Protfolio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active text-light" href="#">About</a>
+        </li>
+       
+        <li className="nav-item">
+          <a className="nav-link active text-light" aria-current="page">Contact</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+</>
+}
+export default Navbar;
